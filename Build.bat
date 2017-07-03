1e409db14d00d2405d35775947155f4aa560d1d6
DaSutt <danielsuttor@gmail.com>
2017-07-03 21:21:01 +0200

    Added shaderc to build script

--- Build.bat
+++ Build.bat
@@ -1,9 +1,24 @@
-md ThirdParty
 cd ThirdParty
 git clone https://github.com/g-truc/glm
 git clone https://github.com/ocornut/imgui.git
 git clone https://github.com/nothings/stb.git
+git clone https://github.com/google/shaderc shaderc
+
+cd shaderc/third_party
+git clone https://github.com/google/googletest.git
+git clone https://github.com/google/glslang.git
+git clone https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git spirv-tools/external/spirv-headers
 cd ..
 md bin
 cd bin
 cmake .. -G "Visual Studio 15 2017 Win64"
+cmake --build . --config Debug
+cmake --build . --config Release
+
+cd ..\..\..\
+md bin
+cd bin
+cmake .. -G "Visual Studio 15 2017 Win64"
+cmake --build . --config Debug
+cmake --build . --config Release
